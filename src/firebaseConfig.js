f83b714daf65f579115aa6ad4f56eb75495e1379
Hanniel Anan <77532173+HannielAnan@users.noreply.github.com>
2024-11-22 14:20:27 +0500

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore"; // Import Firestore
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCtqCJEeq_ipGfe3THvtiZ_NgW-q98e8bw",
+  apiKey: "",
   authDomain: "reggieanddro1234.firebaseapp.com",
   projectId: "reggieanddro1234",
   storageBucket: "reggieanddro1234.firebasestorage.app",
   messagingSenderId: "97956027942",
-  appId: "1:97956027942:web:d02712cbac9bbbf55610e7"
+  appId: ""
 };
 
 // Initialize Firebase
